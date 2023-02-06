a0f37c338b9541419d69e14d994db4cb43bd6afe
Paulina Lestari Simatupang <paulinalestarisimatupang@gmail.com>
2023-02-06 10:16:33 +0700

    p

---
+++ pangkat.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pangkat.h"
+
+int main (){
+	pangkat2();
+}
+
+int pangkat (int x, int y){
+	int p;
+	int hasil=1;
+	
+	 for(p=1;p<=y;p++){
+        hasil=hasil*x;
+    }
+    return hasil;
+}
+
+void pangkat2 (){
+	int x,y;
+	int hasil;
+	printf("Masukkan angka   : ");
+    scanf("%i", &x);
+    printf("Pangkat          : ");
+    scanf("%i", &y);
+ 
+ 	hasil=pangkat ( x,  y);
+    printf("Hasilnya adalah  : %d", hasil);
+}
+
+
---
+++ pangkat.h
@@ -0,0 +1,9 @@
+#ifndef pangkat_h
+#include <stdio.h>
+#include <stdlib.h>
+
+int pangkat (int x, int y);
+
+void pangkat2();
+
+#endif
