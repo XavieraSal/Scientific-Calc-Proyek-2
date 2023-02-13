434cd03f062e0d9601bc3e1dfd461558fc2446a9
Gian Vilcan Patra <gianvilcanpatra@gmail.com>
2023-02-13 07:49:59 +0700

    baru

--- gian.c
+++ gian.c
@@ -1,47 +1,45 @@
 #include <stdio.h>
-#define PI 3.141592654
-#include <math.h>
+#define phi 3.141592654
 #include "gian.h"
-
-int k = 0, result = 0;
+int k = 0, a;
+float result = 0;	
 
 
 void sinus()
 {
-	double a;
+	float a;
     printf("SIN : \nEnter angle in radians: ");
-    scanf("%lf", &a);
-    result = sin(a*PI/180);
-    printf("\nsin dari %g derajat adalah = %.2f",a, result);
+    scanf("%f", &a);
+    result = sin(a*phi/180);
+    printf("\nsin dari derajat adalah = %g", result);
 }
 void cosinus()
 {
     double a;
     printf("Enter angle in radians: ");
     scanf("%lf", &a);
-    result = cos(a*PI/180);
+    result = cos(a*phi/180);
     printf("\ncos dari %g derajat adalah = %.2f",a , result);
 }
-void  Tangen()
+void tangent()
 {
-	double a;
+    double a;
     printf("Enter angle in radians: ");
     scanf("%lf", &a);
-    result = tan(a*PI/180);
-    printf("\ntan dari %g derajat adalah = %.2f",a, result);
+    result = tan(a*phi/180);
+    printf("\nResult = %.2f", result);
 }
-void next()
+void main()
 {
 	switch(menu())
 	{
 		case 1: sinus();
 		break;
 		
-		
 		case 2: cosinus();
 		break;
 		
-		case 3: Tangen();
+		case 3: tangent();
 		break;
 	 } 
 }
@@ -49,7 +47,7 @@ void next()
 int menu()
 {
     int list;
-    printf("\n1. Sin");
+    printf("\n1. Sinus");
     printf("\n2. Cosin");
     printf("\n3. Tangen");
     printf("\n\nmenu : ");
