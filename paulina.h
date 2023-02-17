fa5f2766955df303a318c1f4ca3ed001564ad9aa
Paulina Lestari Simatupang <paulinalestarisimatupang@gmail.com>
2023-02-17 13:30:15 +0700

    no message

--- paulina.h
+++ paulina.h
@@ -1,11 +1,16 @@
 #ifndef paulina_h
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int pangkat (int x, int y);
-float akarkuadrad(float a);
+int akarkuadrad(int a);
+float logBasis10(double n);
+int akarkubik();
 
 void pangkat2();
-void akar();
+void akardua();
+void logBiasa();
+void akartiga();
 
 #endif
