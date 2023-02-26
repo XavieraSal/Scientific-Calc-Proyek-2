e964e14e1a92d1d7867f76429f530318710b7fb1
Paulina Lestari Simatupang <paulinalestarisimatupang@gmail.com>
2023-02-26 10:44:28 +0700

    no message

--- paulina.h
+++ paulina.h
@@ -3,15 +3,58 @@
 #include <stdlib.h>
 #include <math.h>
 
-int pangkat (int x, int y);
-float akar(int angka, int p);
-float logBasis10(double n);
-int nilaimutlak(int x);
+int pangkat (int x, int y)
+{
+	int p;
+	int hasil=1;
+	for(p=1;p<=y;p++){
+		hasil=hasil*x;
+    }
+    return hasil;
+}
 
-void pilihan();
-void pangkat2();
-void akarsemua();
-void logBiasa();
-void mutlak();
+int pangkat2(int x, int y){
+	int hasil;
+
+	hasil = pangkat(x, y);
+    return hasil;
+}
+
+float akar(int angka, int p)
+{	
+	float hasil;
+	hasil = pow (angka, 1.0/p);
+	return hasil;
+}
+
+int akarsemua(int x, int y)
+{ 
+	int angka, p;
+	float hasil;
+    hasil = akar(angka, p);
+    return hasil;
+}
+
+int nilaimutlak(int x)
+{
+	int hasil;
+	hasil = abs(x);
+	return hasil;    
+}
+
+float logBasis10(float n)
+{
+	return log10(n);
+}
+
+float logBiasa(float n)
+{
+	float hasil;
+	
+    hasil = logBasis10 (n);
+    return hasil;
+}
 
 #endif
+
+
