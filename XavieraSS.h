bb7272100528cf4edd69a06d9ddd77031a85e47e
DESKTOP-CP4IIN8\Dell <salsabeelvira@gmail.com>
2023-02-27 05:35:00 +0700

    no message

--- XavieraSS.h
+++ XavieraSS.h
@@ -3,18 +3,34 @@
 #include <math.h>
 #define phi 3.141592654
 
-void InversSin ();
-float InversSinRad(double n);
-float InversSinDrjt (double n);
+float InversSinRad(double n){
+	return asin(n);
+}
 
-void InversCos();
-float InversCosRad(double n);
-float InversCosDrjt (double n);
+float InversSinDrjt (double n){
+	return asin(n)*180/phi;
+}
 
-void InversTan();
-float InversTanRad(double n);
-float InversTanDrjt (double n);
+float InversCosRad(double n){
+	return acos(n);
+}
 
-void logNatural ();
-float logaritmaNatural (double n);
+float InversCosDrjt (double n){
+	return acos(n)*180/phi;
+}
+
+float InversTanRad(double n){
+	return atan(n);
+}
+
+float InversTanDrjt (double n){
+	return (atan(n) * 180) / phi;
+}
+
+float logaritmaNatural (double n){
+	return log(n);
+}
+
+double koefTurunan (double ekspo, double koef);
+double ekspoTurunan (double ekspo);
 #endif
