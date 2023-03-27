6a78ada0e98074e64f63c61fb08633f186ccdb20
Paulina Lestari Simatupang <paulinalestarisimatupang@gmail.com>
2023-03-27 08:39:55 +0700

    no message

--- paulina.h
+++ paulina.h
@@ -2,49 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int pangkat(int x, int y)
-{
-	int p;
-	int hasil=1;
-	for(p=1;p<=y;p++){  //y pangkat
-		hasil=hasil*x;				
-    }
-    return hasil;
-}
+int pangkat(int x, int y);
+double akar2 (double x);
+int nilaimutlak(int angka);
 
-double akar2 (double x) {
-    double epsilon, hasil = x;
-
-    while ((hasil - x / hasil) > epsilon) {
-        hasil = (hasil + x / hasil) / 2;
-    }
-
-    return hasil;
-}
-
-int mutlak(int angka) {
-   if (angka < 0) {
-      return -angka;
-   }
-   else {
-      return angka;
-   }
-}
-
-
-//float logBasis10(float n)
-//{
-//	return log10(n);
-//}
-
-//float logBiasa(float n)
-//{
-//	float hasil;
-//	
-//    hasil = logBasis10 (n);
-//    return hasil;
-//}
+void pilihan();
+void perpangkatan();
+void akar();
+void mutlak();
 
 #endif
-
-
