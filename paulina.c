9f17d757a6987ec2d5e9e65e15740046e75487c0
Paulina Lestari Simatupang <paulinalestarisimatupang@gmail.com>
2023-04-03 09:52:06 +0700

    no message

--- paulina.c
+++ paulina.c
@@ -1,6 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "paulina.h"
 
-<<<<<<< HEAD
 int main (void){
 	pilihan();
 }
@@ -18,6 +19,9 @@ void pilihan(){
 	printf("\t\t\t\t\t                                    ----------------                                      \n");
 	printf("\t\t\t\t\t                                   (3) Mutlak                                             \n");
 	printf("\t\t\t\t\t                                    ----------------                                      \n");
+	printf("\t\t\t\t\t                                    ----------------                                      \n");
+	printf("\t\t\t\t\t                                   (4) Logaritma 10                                       \n");
+	printf("\t\t\t\t\t                                    ----------------                                      \n");
 	printf("\t\t\t\t\t ==================================================================================       \n");
 	printf("\t\t\t\t\t                                       PILIHAN : ");
 	scanf("%d", &menu);
@@ -34,6 +38,9 @@ void pilihan(){
 		case 3:
 			mutlak();
 			break;
+//		case 4:
+//			logBiasa();
+//			break;
 		default:
 			printf("\t\t\t\t\t Mohon maaf pilihan anda salah! ");
 			getche();
@@ -55,15 +62,82 @@ void perpangkatan()
 {
 	int x,y;
 	int hasil;
-=======
-int main(){
-	char operasi[10];
-	float angka;
->>>>>>> 5f535f3ac23885fab1af94d71fb5fcab0d689df7
 	
-	scanf("%s %f", operasi, &angka);
-	if(strstr(operasi, "log")){
-		sscanf(operasi, "log %f", &angka);
-		return logBiasa(angka);
-	}
+	printf(" Perhitungan Pangkat \n");
+	printf("Masukkan angka   : ");
+    scanf("%d", &x);
+    printf("Pangkat          : ");
+    scanf("%d", &y);
+	hasil = pangkat(x, y);
+    printf("Hasilnya adalah  : %d", hasil);
 }
+
+double akar2 (double x)
+{
+    double epsilon, hasil = x;
+
+    while ((hasil - x / hasil) > epsilon) {
+        hasil = (hasil + x / hasil) / 2;
+    }
+
+    return hasil;
+}
+
+void akar()
+{
+	double x;
+	float hasil;
+    printf("Masukkan angka: ");
+    scanf("%lf", &x);
+    hasil = akar2(x);
+    printf("Akar dari %.2f adalah %.6f\n", x, hasil);
+}
+
+int nilaimutlak(int angka) 
+{
+   if (angka < 0) {
+      return -angka;
+   }
+   else {
+      return angka;
+   }
+}
+   
+void mutlak()
+{
+	int angka;
+   	printf("Masukkan angka: ");
+  	scanf("%d", &angka);
+	printf("Nilai mutlak dari %d adalah %d", angka, nilaimutlak(angka));
+}
+
+//#include "paulina.h"
+//
+//int main(){
+////	char operasi[10];
+////	float angka, n;
+//	int nilai, angka;
+//	double x;
+//	int p, y, hasil;
+//	
+//	printf("Masukkan angka: ");
+//    scanf("%d", &p);
+//    printf("Masukkan pangkat: ");
+//    scanf("%d", &y);
+//    hasil = pangkat(p, y);
+//	printf("%d pangkat %d adalah %d\n", p, y, hasil);
+//    return 0;
+//    
+////	printf("Masukkan angka: ");
+////    scanf("%lf", &x);
+////	printf("Akar dari %.2f adalah %.6f\n", x, akar2(x));
+////     return 0;
+//   
+//   printf("Masukkan angka: ");
+//   scanf("%d", &angka);
+//   nilai = mutlak(angka);
+//   printf("Nilai mutlak dari %d adalah %d", angka, mutlak(angka));
+//
+//   return 0;
+//}
+
--- paulina.h
+++ paulina.h
@@ -12,3 +12,54 @@ void akar();
 void mutlak();
 
 #endif
+
+//#ifndef paulina_h
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int pangkat(int x, int y)
+//{
+//	int p;
+//	int hasil=1;
+//	for(p=1;p<=y;p++){  //y pangkat
+//		hasil=hasil*x;				
+//    }
+//    return hasil;
+//}
+//
+//double akar2 (double x) {
+//    double epsilon, hasil = x;
+//
+//    while ((hasil - x / hasil) > epsilon) {
+//        hasil = (hasil + x / hasil) / 2;
+//    }
+//
+//    return hasil;
+//}
+//
+//int mutlak(int angka) {
+//   if (angka < 0) {
+//      return -angka;
+//   }
+//   else {
+//      return angka;
+//   }
+//}
+//
+//
+////float logBasis10(float n)
+////{
+////	return log10(n);
+////}
+//
+////float logBiasa(float n)
+////{
+////	float hasil;
+////	
+////    hasil = logBasis10 (n);
+////    return hasil;
+////}
+//
+//#endif
+
+
